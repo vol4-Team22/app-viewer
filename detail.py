955d8086f13bb3981bf9050f505571cac6e3609b
Tetracontane <144820358+tetracontinental@users.noreply.github.com>
2024-04-27 17:43:41 +0900

    moving style.css

--- detail.py
+++ detail.py
@@ -1,16 +1,9 @@
 import streamlit as st
 
 btn = st.button("←")
-
 st.title("概要・題名")
 content = st.text_input("", max_chars=None)
 
-# CSSスタイルの設定
-style = """
-<style>
-    .title {font-size:20px}
-    .input {font-size:16px; background-color: #FFFACD; border:none}
-    .btn {background-color: #FFA500; color: white; border-radius:50%}
-</style>
-"""
-st.markdown(style, unsafe_allow_html=True)
+# style.cssファイルからスタイルを読み込む
+with open(r"C:\Users\user\Documents\GitHub\mikke-viewer\app\style.css") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
